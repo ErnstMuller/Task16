4eb2ef1990e83b4c9cc2ec7d648a00fc1e5ecca7
ErnstMuller <kentukki2005@gmail.com>
2025-06-15 20:18:27 +0500

    Добавьте файлы проекта.

---
+++ Fraction/FractionStruct.cs
@@ -0,0 +1,108 @@
+﻿using System;
+
+namespace FractionStruct
+{
+    public struct Fraction
+    {
+        public int P { get; set; }
+        private int q;
+
+        public int Q
+        {
+            get => q;
+            set
+            {
+                if (value <= 0)
+                    throw new ArgumentException("Знаменатель должен быть положительным числом");
+                q = value;
+            }
+        }
+
+        public double Value => (double)P / Q;
+
+        public Fraction(int p, int q) : this()
+        {
+            P = p;
+            Q = q;
+        }
+
+        public override string ToString() => $"{P}/{Q}";
+
+        public override bool Equals(object obj)
+        {
+            if (obj is Fraction other)
+            {
+                return P * other.Q == other.P * Q;
+            }
+            throw new ArgumentException("Объект для сравнения не является дробью");
+        }
+
+        public override int GetHashCode()
+        {
+            int gcd = GCD(Math.Abs(P), Math.Abs(Q));
+            int simplifiedP = P / gcd;
+            int simplifiedQ = Q / gcd;
+
+            unchecked
+            {
+                int hash = 17;
+                const int prime = 23;
+                hash = hash * prime + simplifiedP.GetHashCode();
+                hash = hash * prime + simplifiedQ.GetHashCode();
+                return hash;
+            }
+        }
+
+        private static int GCD(int a, int b)
+        {
+            while (b != 0)
+            {
+                int temp = b;
+                b = a % b;
+                a = temp;
+            }
+            return a;
+        }
+
+        public static bool operator ==(Fraction left, Fraction right) => left.Equals(right);
+        public static bool operator !=(Fraction left, Fraction right) => !left.Equals(right);
+
+        public static Fraction operator +(Fraction left, Fraction right)
+        {
+            int newP = left.P * right.Q + right.P * left.Q;
+            int newQ = left.Q * right.Q;
+            return new Fraction(newP, newQ);
+        }
+
+        public static Fraction operator -(Fraction left, Fraction right)
+        {
+            int newP = left.P * right.Q - right.P * left.Q;
+            int newQ = left.Q * right.Q;
+            return new Fraction(newP, newQ);
+        }
+
+        public static Fraction operator *(Fraction left, Fraction right)
+        {
+            int newP = left.P * right.P;
+            int newQ = left.Q * right.Q;
+            return new Fraction(newP, newQ);
+        }
+
+        public static Fraction operator /(Fraction left, Fraction right)
+        {
+            if (right.P == 0)
+                throw new DivideByZeroException("Деление на нулевую дробь невозможно");
+
+            int newP = left.P * right.Q;
+            int newQ = left.Q * right.P;
+
+            if (newQ < 0)
+            {
+                newP = -newP;
+                newQ = -newQ;
+            }
+
+            return new Fraction(newP, newQ);
+        }
+    }
+}
---
+++ FractionStruct.UnitTests/FractionStructTests.cs
@@ -0,0 +1,147 @@
+using NUnit.Framework;
+using FractionStruct;
+
+namespace FractionStruct.UnitTests
+{
+    [TestFixture]
+    public class FractionTests
+    {
+        [Test]
+        public void ConstructorTest()
+        {
+            var fraction = new Fraction(3, 4);
+            Assert.That(fraction.P, Is.EqualTo(3));
+            Assert.That(fraction.Q, Is.EqualTo(4));
+        }
+
+        [Test]
+        public void QSet_NonPositiveValue_ArgumentException()
+        {
+            var fraction = new Fraction();
+            Assert.That(() => fraction.Q = 0, Throws.ArgumentException);
+            Assert.That(() => fraction.Q = -5, Throws.ArgumentException);
+        }
+
+        [Test]
+        public void ValueTest()
+        {
+            var fraction1 = new Fraction(1, 2);
+            Assert.That(fraction1.Value, Is.EqualTo(0.5).Within(1e-13));
+
+            var fraction2 = new Fraction(-3, 4);
+            Assert.That(fraction2.Value, Is.EqualTo(-0.75).Within(1e-13));
+
+            var fraction3 = new Fraction(0, 1);
+            Assert.That(fraction3.Value, Is.EqualTo(0).Within(1e-13));
+        }
+
+        [TestCase(1, 2, "1/2")]
+        [TestCase(-3, 4, "-3/4")]
+        [TestCase(0, 1, "0/1")]
+        public void ToStringTest(int p, int q, string result)
+        {
+            var fraction = new Fraction(p, q);
+            Assert.That(fraction.ToString(), Is.EqualTo(result));
+        }
+
+        [TestCase(1, 2, 2, 4, true)]
+        [TestCase(1, 2, 3, 6, true)]
+        [TestCase(1, 2, 1, 3, false)]
+        [TestCase(-1, 2, -2, 4, true)]
+        public void Equals_TwoFractions_ExpectedResult(int p1, int q1, int p2, int q2, bool result)
+        {
+            var fraction1 = new Fraction(p1, q1);
+            var fraction2 = new Fraction(p2, q2);
+            Assert.That(fraction1.Equals(fraction2), Is.EqualTo(result));
+        }
+
+        [Test]
+        public void Equals_WrongArgument_ArgumentException()
+        {
+            var fraction = new Fraction(1, 2);
+            var obj = new object();
+            Assert.That(() => fraction.Equals(obj), Throws.ArgumentException);
+        }
+
+        [Test]
+        public void GetHashCodeTest()
+        {
+            var x = new Fraction(1, 2);
+            var y = new Fraction(2, 4);
+            var z = new Fraction(1, 3);
+
+            Assert.That(x.GetHashCode(), Is.EqualTo(y.GetHashCode()));
+            Assert.That(x.GetHashCode(), Is.Not.EqualTo(z.GetHashCode()));
+        }
+
+        [Test]
+        public void ComparisonTest()
+        {
+            var x = new Fraction(1, 2);
+            var y = new Fraction(2, 4);
+            var z = new Fraction(1, 3);
+
+            Assert.That(x == y, Is.True);
+            Assert.That(x != y, Is.False);
+            Assert.That(x == z, Is.False);
+            Assert.That(x != z, Is.True);
+        }
+
+        [TestCase(1, 2, 1, 3, 5, 6)]
+        [TestCase(1, 2, -1, 4, 1, 4)]
+        [TestCase(-1, 3, -1, 6, -1, 2)]
+        public void AdditionTest(int p1, int q1, int p2, int q2, int resultP, int resultQ)
+        {
+            var fraction1 = new Fraction(p1, q1);
+            var fraction2 = new Fraction(p2, q2);
+            var result = new Fraction(resultP, resultQ);
+
+            Assert.That(fraction1 + fraction2, Is.EqualTo(result));
+        }
+
+        [TestCase(1, 2, 1, 3, 1, 6)]
+        [TestCase(1, 2, -1, 4, 3, 4)]
+        [TestCase(-1, 3, -1, 6, -1, 6)]
+        public void SubtractionTest(int p1, int q1, int p2, int q2, int resultP, int resultQ)
+        {
+            var fraction1 = new Fraction(p1, q1);
+            var fraction2 = new Fraction(p2, q2);
+            var result = new Fraction(resultP, resultQ);
+
+            Assert.That(fraction1 - fraction2, Is.EqualTo(result));
+        }
+
+        [TestCase(1, 2, 2, 3, 1, 3)]
+        [TestCase(3, 4, -2, 5, -3, 10)]
+        [TestCase(-1, 3, -3, 4, 1, 4)]
+        public void MultiplicationTest(int p1, int q1, int p2, int q2, int resultP, int resultQ)
+        {
+            var fraction1 = new Fraction(p1, q1);
+            var fraction2 = new Fraction(p2, q2);
+            var result = new Fraction(resultP, resultQ);
+
+            Assert.That(fraction1 * fraction2, Is.EqualTo(result));
+        }
+
+        [TestCase(1, 2, 2, 3, 3, 4)]
+        [TestCase(3, 4, -2, 5, -15, 8)]
+        [TestCase(-1, 3, -3, 4, 4, 9)]
+        public void DivisionTest(int p1, int q1, int p2, int q2, int resultP, int resultQ)
+        {
+            var fraction1 = new Fraction(p1, q1);
+            var fraction2 = new Fraction(p2, q2);
+            var result = new Fraction(resultP, resultQ);
+
+            Assert.That(fraction1 / fraction2, Is.EqualTo(result));
+        }
+
+        [Test]
+        public void Division_ByZero_DivideByZeroException()
+        {
+            var fraction1 = new Fraction(1, 2);
+            var fraction2 = new Fraction(0, 1);
+
+            Assert.That(() => fraction1 / fraction2, Throws.TypeOf<DivideByZeroException>());
+        }
+    }
+}
